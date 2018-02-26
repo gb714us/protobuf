b26c42affbf353e6f28cbae6ea8b4b76e11056ae
Bo Yang <paulyang1211@gmail.com>
2018-02-26 10:16:55 -0800

    Add mingw

--- appveyor.bat
+++ appveyor.bat
@@ -1,5 +1,6 @@
 setlocal
 
+echo %PATH%
 pip install wheel
 cd %REPO_DIR%\python
 git checkout %BUILD_COMMIT%
@@ -7,4 +8,8 @@ sed -i 's/\[\'-Wno-write-strings\',/\[\]/g' setup.py
 sed -i '/Wno-invalid-offsetof/d' setup.py
 sed -i '/Wno-sign-compare/d' setup.py
 cat setup.py
+dir %MINGW%
+set path
+gcc
+%MINGW%\gcc
 python setup.py bdist_wheel --cpp_implementation --compile_static_extension
